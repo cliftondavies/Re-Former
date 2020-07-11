1dc5770893c10cd403420523fc1ae155ce9632a2
cliftondavies <clifton.davies@outlook.com>
2020-07-11 14:42:21 +0100

    Implement #update route and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
   private
 
   def user_params
